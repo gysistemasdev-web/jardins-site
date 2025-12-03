b3015069291fc9cd6be3765c141f5c073457ad72
Gabryell <Gabryell@ACELNET.LOCAL>
2025-12-03 15:56:37 -0300

    Fix Vite base for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/jardins-site/', // 👈 adicionado para o GitHub Pages
+  base: './', // 👈 CORRETO PARA VERCEL
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
