a3d223569b8c30e197c3295cbd477a88de5df431
Gabryell <Gabryell@ACELNET.LOCAL>
2025-12-08 15:59:10 -0300

    fix: vite base config for Hostinger deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 CORRETO PARA VERCEL
+  base: '/', // obrigatório para deploy no Hostinger
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
-
